bf5a57a8123edc9abe6def2fa8980885f599ce63
ashrqat-fawzy <ashrqat.fawzy@gmail.com>
2023-09-18 17:06:23 +0300

    a

--- string_fields.c
+++ string_fields.c
@@ -1,22 +1,14 @@
 #include "main.h"
-<<<<<<< HEAD
-=======
 
->>>>>>> 312d7c07216ff0a01745273a1bc9d83e0deda19e
 /**
  * get_precision - gets the precision from the format string
  * @p: the format string
  * @params: the parameters struct
  * @ap: the argument pointer
-<<<<<<< HEAD
- * Return: new pointer
- */
-
-=======
  *
  * Return: new pointer
  */
->>>>>>> 312d7c07216ff0a01745273a1bc9d83e0deda19e
+
 char *get_precision(char *p, params_t *params, va_list ap)
 {
 	int d = 0;
@@ -24,10 +16,7 @@ char *get_precision(char *p, params_t *params, va_list ap)
 	if (*p != '.')
 		return (p);
 	p++;
-<<<<<<< HEAD
 
-=======
->>>>>>> 312d7c07216ff0a01745273a1bc9d83e0deda19e
 	if (*p == '*')
 	{
 		d = va_arg(ap, int);
@@ -41,7 +30,3 @@ char *get_precision(char *p, params_t *params, va_list ap)
 	params->precision = d;
 	return (p);
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 312d7c07216ff0a01745273a1bc9d83e0deda19e
