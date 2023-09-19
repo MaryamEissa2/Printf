67888d32fa569200775f6cd457f5aa33289cc6a5
MaryamEissa2 <maryameissa82003@gmail.com>
2023-09-19 11:26:58 -0700

    maryam

--- Convert_Nums.c
+++ Convert_Nums.c
@@ -9,19 +9,19 @@
  */
 int print_hex(va_list ap, params_t *params)
 {
-	unsigned long l;
+	unsigned long lon;
 	int c = 0;
 	char *str;
 
-	if (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
+	if (params->lon_modifier)
+		lon = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
+		lon = (unsigned short int)va_arg(ap, unsigned int);
 	else
-		l = (unsigned int)va_arg(ap, unsigned int);
+		lon = (unsigned int)va_arg(ap, unsigned int);
 
-	str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
-	if (params->hashtag_flag && l)
+	str = convert(lon, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	if (params->hashtag_flag && lon)
 	{
 		*--str = 'x';
 		*--str = '0';
@@ -39,19 +39,19 @@ int print_hex(va_list ap, params_t *params)
  */
 int print_HEX(va_list ap, params_t *params)
 {
-	unsigned long l;
+	unsigned long lon;
 	int c = 0;
 	char *str;
 
-	if (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
+	if (params->lon_modifier)
+		lon = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
+		lon = (unsigned short int)va_arg(ap, unsigned int);
 	else
-		l = (unsigned int)va_arg(ap, unsigned int);
+		lon = (unsigned int)va_arg(ap, unsigned int);
 
-	str = convert(l, 16, CONVERT_UNSIGNED, params);
-	if (params->hashtag_flag && l)
+	str = convert(lon, 16, CONVERT_UNSIGNED, params);
+	if (params->hashtag_flag && lon)
 	{
 		*--str = 'X';
 		*--str = '0';
@@ -68,11 +68,11 @@ int print_HEX(va_list ap, params_t *params)
  */
 int print_binary(va_list ap, params_t *params)
 {
-	unsigned int n = va_arg(ap, unsigned int);
+	unsigned int num = va_arg(ap, unsigned int);
 	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
 	int c = 0;
 
-	if (params->hashtag_flag && n)
+	if (params->hashtag_flag && num)
 		*--str = '0';
 	params->unsign = 1;
 	return (c += print_number(str, params));
@@ -87,19 +87,19 @@ int print_binary(va_list ap, params_t *params)
  */
 int print_octal(va_list ap, params_t *params)
 {
-	unsigned long l;
+	unsigned long lon;
 	char *str;
 	int c = 0;
 
-	if (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
+	if (params->lon_modifier)
+		lon = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
+		lon = (unsigned short int)va_arg(ap, unsigned int);
 	else
-		l = (unsigned int)va_arg(ap, unsigned int);
-	str = convert(l, 8, CONVERT_UNSIGNED, params);
+		lon = (unsigned int)va_arg(ap, unsigned int);
+	str = convert(lon, 8, CONVERT_UNSIGNED, params);
 
-	if (params->hashtag_flag && l)
+	if (params->hashtag_flag && lon)
 		*--str = '0';
 	params->unsign = 1;
 	return (c += print_number(str, params));
